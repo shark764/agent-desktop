d51666b1a05d545ecfd53da73745c8430cfb1b4f
Jon Millar <xesive@gmail.com>
2016-12-14 17:07:50 -0400

    update styles

--- app/components/A/index.js
+++ app/components/A/index.js
@@ -4,13 +4,21 @@
 *
 */
 
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 function A(props) {
   return (
-    <div {...props}>
-    </div>
+    <span style={Object.assign({}, { textDecoration: 'underline', color: '#494949' }, props.style)}>
+      <FormattedMessage {...props.text} />
+    </span>
   );
 }
+
+A.propTypes = {
+  style: PropTypes.object,
+  text: PropTypes.object,
+};
+
 
 export default A;
