b6f7444be2c17f65bd4e1d6a2686356af73e7054
Josh Clowater <jclowater@liveopscloud.com>
2016-12-16 14:49:12 -0400

    Added actions to set tenantId, presence, direction, and available presences on tenant select.

--- app/containers/AgentDesktop/actions.js
+++ app/containers/AgentDesktop/actions.js
@@ -5,11 +5,36 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  SET_TENANT_ID,
+  SET_DIRECTION,
+  SET_PRESENCE,
+  SET_AVAILABLE_PRESENCES,
 } from './constants';
 
-export function defaultAction() {
+export function setTenantId(tenantId) {
   return {
-    type: DEFAULT_ACTION,
+    type: SET_TENANT_ID,
+    tenantId,
+  };
+}
+
+export function setDirection(direction) {
+  return {
+    type: SET_DIRECTION,
+    direction,
+  };
+}
+
+export function setPresence(presence) {
+  return {
+    type: SET_PRESENCE,
+    presence,
+  };
+}
+
+export function setAvailablePresences(presences) {
+  return {
+    type: SET_AVAILABLE_PRESENCES,
+    presences,
   };
 }
