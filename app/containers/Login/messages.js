d36461c690b10b0faefe9e3efbef133b0acc2c58
Jon Millar <xesive@gmail.com>
2017-02-15 16:33:11 -0400

    update legal and copyright to use Serenova instead of Liveops cloud

--- app/containers/Login/messages.js
+++ app/containers/Login/messages.js
@@ -64,11 +64,11 @@ export default defineMessages({
   },
   copyright: {
     id: 'app.containers.Login.copyright',
-    defaultMessage: 'Copyright © 2015-{year} LiveOps Cloud LLC. All rights reserved.',
+    defaultMessage: 'Copyright © 2015-{year} Serenova, LLC (“Serenova’). All rights reserved.',
     values: { year: new Date().getFullYear() },
   },
   legal: {
     id: 'app.containers.Login.legal',
-    defaultMessage: 'Access to this site requires separate permission from LiveOps Cloud LLC. This site contains confidential information, and may also contain content and enable interaction with users from third-party sites subject to different terms that are outside of LiveOps Cloud\'s control. By using or accessing this site, you have agreed to the Terms of Service as outlined in the Master Service Agreement ("Agreement"). A copy of this Agreement is available from your LiveOps Cloud contact.',
+    defaultMessage: 'Access to this site requires separate permission from Serenova, LLC. This site contains confidential information, and may also contain content and enable interaction with users from third-party sites subject to different terms that are outside of Serenova\'s control. By using or accessing this site, you have agreed to the Terms of Service as outlined in the Master Service Agreement ("Agreement"). A copy of this Agreement is available from your Serenova contact.',
   },
 });
