c97d8883051659bb8d14ea8f9cb5a3e22b082a5f
Jon Millar <xesive@gmail.com>
2017-02-16 14:37:11 -0400

    update legalise font size

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -242,9 +242,9 @@ export class Login extends React.Component {
       top: '-406.4px',
     },
     copyright: {
-      width: '550px',
+      width: '65vw',
       position: 'relative',
-      bottom: '-5em',
+      bottom: '-10em',
       color: '#FFFFFF',
       textAlign: 'center',
     },
