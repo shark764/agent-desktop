449f0cacd1813387601d656f256a6b4b06016dbf
nickguimond <nguimond@serenova.com>
2017-10-05 16:46:08 -0300

    agent gets error if they try to login to disabled default tenant

--- app/containers/Errors/messages.js
+++ app/containers/Errors/messages.js
@@ -35,6 +35,11 @@ export default defineMessages({
     id: 'app.containers.Errors.AD-1004',
     defaultMessage: 'You have insufficient permissions to access tenant.',
   },
+  'AD-1005': {
+    id: 'app.containers.Errors.AD-1005',
+    defaultMessage:
+      'This account is not currently associated with an active tenant. Please reach out to your system administrator for assistance.',
+  },
   2001: {
     id: 'app.containers.Errors.2001',
     defaultMessage: 'Failed to get user configuration. Please try again.',
