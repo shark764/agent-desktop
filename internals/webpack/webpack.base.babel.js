84af7113f499b20ba3087e644b986da15bf1e7f0
Josh Clowater <jclowater@liveopscloud.com>
2017-12-05 14:38:21 -0400

    CXV1-12283 - SDK bump to fix MQTT not ending on wrapup-started

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -86,6 +86,9 @@ module.exports = (options) => ({
     new webpack.NamedModulesPlugin(),
   ]),
   resolve: {
+    alias: {
+      'moment$': 'moment/moment',
+    },
     modules: (options.resolve && options.resolve.modules) || ['app', 'node_modules'],
     extensions: [
       '.js',
